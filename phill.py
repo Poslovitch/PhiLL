5df4505bf00309075c5323e95732811ed610dd4f
Florian CUNY <poslovitch@bentobox.world>
2020-03-31 21:00:02 +0200

    Removed unused function in phill.py

--- phill.py
+++ phill.py
@@ -1,28 +1,6 @@
-import pywikibot as pwb
-import wikitextparser as wtp
 from PhillApp import *
 
 VERSION = "0.0.1"
-
-def complete_task(ipa):
-    task = tasks[0]
-    page = pwb.Page(site, task[0])
-
-    wikicode = wtp.parse(page.text)
-    for section in wikicode.sections:
-        if section.title.replace(" ", "").lower() == "{{langue|fr}}":
-            for template in section.templates:
-                for arg in template.arguments:
-                    if arg.name.lower() == "audio" and arg.value == task[1]:
-                        template.set_arg("pron", ipa)
-                        break
-
-    page.text = str(wikicode)
-
-    page.save(SUMMARY, minor=False)
-
-    tasks.remove(task)
-
 
 # GUI:
 app = QApplication([])
